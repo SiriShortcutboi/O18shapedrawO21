a8441ab9689d8dd74449598e27e3ad497011b0e1
SiriShortcutboi <58081965+SiriShortcutboi@users.noreply.github.com>
2026-03-29 14:14:34 -0600

    Assignment 18 commit

--- ShapePolymorphismO21/Program.cs
+++ ShapePolymorphismO21/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+    
+       
+            Circle circle = new Circle();
+            Square square = new Square();
+            Triangle triangle = new Triangle();
+       
+        
+            Console.WriteLine(circle.Draw()); 
+            Console.WriteLine(triangle.Draw()); 
+            Console.WriteLine(square.Draw()); 
+
+        }
+    }
+
+
+public abstract class Shape
+{
+    protected abstract string Name { get; }
+
+    public string Draw()
+    {
+        return $"Shaper is drawing a {Name}";
+    }
+}
+
+public class Circle : Shape
+{
+    protected override string Name => "circle";
+}
+
+public class Square : Shape
+{
+    protected override string Name => "square";
+}
+
+public class Triangle : Shape
+{
+    protected override string Name => "triangle";
+}
